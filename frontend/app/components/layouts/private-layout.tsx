8d9f32c3720d8aaf096a7c54edbd078e625122d7
Jannik Köster <koesterjannik1998@gmail.com>
2025-04-16 17:10:52 +0200

    add sidebar dashboard

--- frontend/app/components/layouts/private-layout.tsx
+++ frontend/app/components/layouts/private-layout.tsx
@@ -3,6 +3,10 @@ import SiteHeader from '../navigation/public/siteHeader'
 import { useUser } from '@/hooks/useUser'
 import { useNavigate } from 'react-router';
 import { useUserStore } from '@/stores/userUserStore';
+import { SidebarInset, SidebarProvider, SidebarTrigger } from '../ui/sidebar';
+import { AppSidebar } from '../app-sidebar';
+import { Separator } from '../ui/separator';
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from '../ui/breadcrumb';
 
 type Props = {
     children: React.ReactNode
@@ -25,10 +29,26 @@ function PrivateLayout({ children }: Props) {
     }
   }, [query.data?.data?.user, query.isError, query.isSuccess]);
   return (
-    <div className='container mx-auto'>
-       
-        {children}
-    </div>
+    <SidebarProvider>
+    <AppSidebar />
+    <SidebarInset>
+      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          {children}
+        </div>
+      </header>
+      <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+          <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="aspect-video rounded-xl bg-muted/50" />
+          <div className="aspect-video rounded-xl bg-muted/50" />
+        </div>
+        <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      </div>
+    </SidebarInset>
+  </SidebarProvider>
   )
 }
 
